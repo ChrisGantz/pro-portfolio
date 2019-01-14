00ed71082e4ce1677198d0026f35b192d32c69c7
Chris Gantz <chris.gonzalez836@gmail.com>
2019-01-13 19:27:41 -0700

    can copy email on click

--- main.js
+++ main.js
@@ -4,5 +4,18 @@ $(document).ready(function () {
 
   $("#fade-first").fadeIn(2000);
   $("#fade-second").fadeIn(3000);
+  // copy email to clip-board
+  $(".copy-email").on({
+    click: e => document.execCommand("copy"),
+    copy: e => {
+      if (e.originalEvent.clipboardData) {
+        e.originalEvent.clipboardData.setData("text/plain", "$(this)[0].title");
+        alert("Email copied to clipboard!");
+        e.preventDefault();
+      } else {
+        alert("Could not be copied");
+      }
+    }
+  });
 
 });
