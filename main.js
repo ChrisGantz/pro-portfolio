ac175d04fdd413260be31110b4fc677db6cd3fef
Chris Gantz <chris.gonzalez836@gmail.com>
2019-03-04 12:24:41 -0700

    hamburger menu added

--- main.js
+++ main.js
@@ -2,20 +2,22 @@
 
 $(document).ready(function () {
 
-  $("#fade-first").fadeIn(2000);
-  $("#fade-second").fadeIn(3000);
+  $('#fade-first').fadeIn(2000);
+  $('#fade-second').fadeIn(3000);
   // copy email to clip-board
-  $(".copy-email").on({
-    click: e => document.execCommand("copy"),
+  $('.copy-email').on({
+    click: e => document.execCommand('copy'),
     copy: e => {
       if (e.originalEvent.clipboardData) {
-        e.originalEvent.clipboardData.setData("text/plain", "$(this)[0].title");
-        alert("Email copied to clipboard!");
+        e.originalEvent.clipboardData.setData('text/plain', '$(this)[0].title');
+        alert('Email copied to clipboard!');
         e.preventDefault();
       } else {
-        alert("Could not be copied");
+        alert('Could not be copied');
       }
     }
   });
+
+  
 
 });
